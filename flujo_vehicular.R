6537b8b12517290e9ad8ad6ca60b9e433ddc8244
Mateo W. Racca <52387817+raccamateo@users.noreply.github.com>
2020-08-10 20:44:00 -0300

    Update flujo_vehicular.R

--- flujo_vehicular.R
+++ flujo_vehicular.R
@@ -25,3 +25,24 @@ head(flujo_vehicular)
 
 #vamos a ver qué tipo de variables tenemos
 str(flujo_vehicular)
+
+
+#transformamos el formato de las fechas a día-mes-año-horas:minutos:segundos
+flujo_vehicular <- flujo_vehicular %>% mutate(FECHA_HORA = dmy_hms(FECHA_HORA))
+
+#ahora, extraemos la hora de los registros (se cierran por hora y tienen info de ingresos, egresos y circulación interna)
+flujo_vehicular$HORA <- hour(flujo_vehicular$FECHA_HORA)
+
+#animación sobre el flujo vehícular. transiciones por día
+ggplot(flujo_vehicular, aes(x = FECHA_HORA, y = CANTIDAD, color = SENTIDO)) +
+  geom_area() +
+  transition_reveal(FECHA_HORA) +
+  labs(title = "Flujo vehicular",
+       subtitle = "Ciudad Autónoma de Buenos Aires",
+       x = "",
+       y = "Vehículos") +
+  scale_color_viridis(discrete=TRUE) +
+  theme_minimal() +
+  theme(legend.position = "bottom")
+
+anim_save("flujo_vehicular.gif")
