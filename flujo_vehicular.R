df79fed5aa0be9d36eeec967eb87fa4a60fe79ec
Mateo W. Racca <52387817+raccamateo@users.noreply.github.com>
2020-08-11 20:21:41 -0300

    Update flujo_vehicular.R

--- flujo_vehicular.R
+++ flujo_vehicular.R
@@ -42,3 +42,37 @@ ggplot(flujo_vehicular, aes(x = FECHA_HORA, y = CANTIDAD, color = SENTIDO)) +
   theme(legend.position = "bottom")
 
 anim_save("flujo_vehicular.gif")
+
+
+
+#vamos a descargar un archivo .geojson con la info sobre las comunas en CABA
+comunas <- st_read('https://bitsandbricks.github.io/data/CABA_comunas.geojson')
+
+
+#chequeamos si hay valores perdidos en el dataset de flujo vehicular
+anyNA(flujo_vehicular)
+
+#como los hay, vamos a descartar las filas que tengan valores NA
+flujo_vehicular <- na.omit(flujo_vehicular)
+
+#Ahora, para poder hacer un mapa, vamos a tomar el LAT-LON y transformarlo en sf
+flujo_vehicular <- st_as_sf(flujo_vehicular, coords = c('LON','LAT'), crs = 4326)
+
+#chequeamos que los datasets estén codificados iguales
+st_crs(flujo_vehicular)
+st_crs(comunas)
+
+#hacemos un mapa animado con los puntos en los que están los radares o sistemas de medición del flujo de vehículos
+#y vemos la evolución (promedio de flujo por semana del año).
+ggplot() +
+  geom_sf(data = comunas) +
+  geom_sf(data = flujo_vehicular, mapping = aes(col = CODIGO_LOCACION, size = CANTIDAD)) +
+  transition_time(SEMANA) +
+  scale_color_viridis(discrete=TRUE) +
+  labs(title = "Flujo vehicular en CABA",
+       subtitle = "Semana {as.integer(frame_time)}",
+       x = "",
+       y = "") +
+  theme_minimal()
+
+anim_save("flujo_vehicular_mapa_semana.gif")
