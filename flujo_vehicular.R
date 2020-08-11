d6ac6cb1f368eb5e68a26dec258cd69d8deb91bb
Mateo W. Racca <52387817+raccamateo@users.noreply.github.com>
2020-08-10 21:11:47 -0300

    Update flujo_vehicular.R

--- flujo_vehicular.R
+++ flujo_vehicular.R
@@ -14,10 +14,6 @@ library(tidyverse)
 options(scipen = 999)
 
 
-#vamos a descargar la data sobre radios censales que está publicada en el githun de bitsandbricks
-radios <- st_read("https://bitsandbricks.github.io/data/CABA_rc.geojson")
-
-
 #ahora vamos a trabajar con datos relacionados al flujo vehicular
 flujo_vehicular <- read.csv('https://cdn.buenosaires.gob.ar/datosabiertos/datasets/transporte/flujo-vehicular-anillo-digital/dataset_flujo_vehicular.csv')
 write_csv(flujo_vehicular, "flujo_vehicular.csv")
