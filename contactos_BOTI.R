c2ba9f0fba90f895a9f5d3a622ddce016438ece4
Mateo W. Racca <52387817+raccamateo@users.noreply.github.com>
2020-08-14 19:40:13 -0300

    Update contactos_BOTI.R

--- contactos_BOTI.R
+++ contactos_BOTI.R
@@ -13,4 +13,14 @@ options(scipen = 999)
 #contactos BOTI/hora
 contactos_BOTI <- read.csv('http://cdn.buenosaires.gob.ar/datosabiertos/datasets/innovacion-transformacion-digital/contactos-boti-triage-covid-19/contactos_boti_triage_covid_19.csv')
 
-contactos_BOTI$hora <- as.hms(hora)
+
+head(contactos_BOTI)
+
+#como la hora de los contactos contiene solo hs y no minutos ni segundos, vamos a agregárselos
+contactos_BOTI$hora <- str_c(contactos_BOTI$hora, "00:00", sep = ":")
+
+#vamos a combinar las columnas fecha y hora para crrar la variable fecha_hora
+contactos_BOTI$fecha_hora <- str_c(contactos_BOTI$fecha, contactos_BOTI$hora)
+
+#transformamos el formato de las fechas a día-mes-año-horas:minutos:segundos
+contactos_BOTI <- contactos_BOTI %>% mutate(fecha_hora = dmy_hms(fecha_hora))
